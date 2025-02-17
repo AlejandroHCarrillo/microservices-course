982379c83f4c7cb69bd87d18438e48b315086129
Alejandro Hernandez Carrillo <el_grande_ahc@hotmail.com>
2025-02-17 09:27:14 -0600

    Adding platform controller and get endpoints

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+            var platformList = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformList));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatform(int id)
+        {
+            Console.WriteLine("--> Getting Platform...");
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
+
+    }
+}
